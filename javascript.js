90c369a12f6208c4f015e945d51e0426774f46c0
rozina-aamir <rozina.aamir@outlook.com>
2024-10-10 10:25:32 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
         return "Rock";
     }
