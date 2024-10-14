48f447a8b1100c326d32bb3be29e739f337ebe6c
rozina-aamir <rozina.aamir@outlook.com>
2024-10-14 11:02:46 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log("The computer says: " + getComputerChoice())
+
+function getHumanChoice() {
+    let humanChoice = window.prompt("Make your choice. Rock, paper, or scissors.");
+    if (humanChoice.toLowerCase() === "rock") {
+        return "Rock";
+    } else if (humanChoice.toLowerCase() === "paper") {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log("You say: " + getHumanChoice())
+
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
