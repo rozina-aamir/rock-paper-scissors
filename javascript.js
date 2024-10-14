2268151e32e4bd25c9e626cc89309ea96170b74a
rozina-aamir <rozina.aamir@outlook.com>
2024-10-14 10:30:40 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -32,39 +32,38 @@ function playGame() {
         console.log("Your score is: " + humanScore);
         console.log("The computer's score is: " + computerScore);
     }
-}
 
-
-function playRound(humanChoice, computerChoice) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    if (humanChoice == "Rock" && computerChoice == "Rock") {
-        console.log("It's a draw!");
-    } else if (humanChoice == "Rock" && computerChoice == "Paper") {
-        console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around.`);
-        computerScore ++;
-    } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-        console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
-        humanScore ++;
-    } else if(humanChoice == "Paper" && computerChoice == "Rock") {
-        console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
-        humanScore ++;
-    } else if(humanChoice == "Paper" && computerChoice == "Paper") {
-        console.log("It's a draw!");
-    } else if(humanChoice == "Paper" && computerChoice == "Scissors") {
-        console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around`);
-        computerScore ++;
-    } else if(humanChoice == "Scissors" && computerChoice == "Rock") {
-        console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around.`);
-        computerScore ++;
-    } else if(humanChoice == "Scissors" && computerChoice == "Paper") {
-        console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
-        humanChoice ++;
-    } else if(humanChoice == "Scissors" && computerChoice == "Scissors") {
-        console.log("It's a draw!");
-    } else {
-        console.log("Please enter a valid answer.");
+    function playRound(humanChoice, computerChoice) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+    
+        if (humanChoice == "Rock" && computerChoice == "Rock") {
+            console.log("It's a draw!");
+        } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+            console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around.`);
+            computerScore ++;
+        } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+            console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
+            humanScore ++;
+        } else if(humanChoice == "Paper" && computerChoice == "Rock") {
+            console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
+            humanScore ++;
+        } else if(humanChoice == "Paper" && computerChoice == "Paper") {
+            console.log("It's a draw!");
+        } else if(humanChoice == "Paper" && computerChoice == "Scissors") {
+            console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around`);
+            computerScore ++;
+        } else if(humanChoice == "Scissors" && computerChoice == "Rock") {
+            console.log(`You loose this round. ${computerChoice} beats ${humanChoice} this time around.`);
+            computerScore ++;
+        } else if(humanChoice == "Scissors" && computerChoice == "Paper") {
+            console.log(`You win this round! ${humanChoice} has won ${computerChoice}!`);
+            humanChoice ++;
+        } else if(humanChoice == "Scissors" && computerChoice == "Scissors") {
+            console.log("It's a draw!");
+        } else {
+            console.log("Please enter a valid answer.");
+        }
     }
 }
 
